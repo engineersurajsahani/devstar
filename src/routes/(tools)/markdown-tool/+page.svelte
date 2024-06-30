e1a450bfec1dd6626ff0f5dc4b3982daa93da8ac
Anand Kumar <anandkumarkharahana@gmail.com>
2024-06-30 15:55:12 +0530

    Anand-Kumar

--- src/routes/(tools)/markdown-tool/+page.svelte
+++ src/routes/(tools)/markdown-tool/+page.svelte
@@ -1,11 +1,20 @@
-<script>
+<script lang="js">
+	// @ts-nocheck
+	import Intro from './Intro.svelte';
+	import Editor from './Editor.svelte';
+	// import './markdown.css';
 
+	// export let data;
 </script>
 
+<!-- <Intro heading={data.meta.title} description={data.meta.description} /> -->
+
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
+	<Editor />
 </div>
 
 <style>
-
+	:is(.dark .card) {
+		box-shadow: rgba(255, 255, 255, 0.5) 0 0 0 2px;
+	}
 </style>

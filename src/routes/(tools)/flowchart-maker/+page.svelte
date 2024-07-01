694642dd54f389a7246ad9518b8fa89b0c1c55aa
Ashwini <140798927+ashwinii19@users.noreply.github.com>
2024-07-01 19:45:57 +0530

    Home Page GUI

--- src/routes/(tools)/flowchart-maker/+page.svelte
+++ src/routes/(tools)/flowchart-maker/+page.svelte
@@ -2,8 +2,31 @@
 
 </script>
 
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
+<div class="card gap-16 items-center mx-auto max-w-screen-xl   overflow-hidden rounded-lg">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<!-- Add tool here -->
+	 <div class="mainbody">
+		<div class="top">
+			<div class="search"></div>
+		</div>
+	
+		<div class="heading">
+			<div class="left">
+				<p><marquee>Streamline Your Workflow with Clarity</marquee></p>
+				<p>The ultimate tool for transforming complex ideas into <br>clear visual representation. Streamline your workflow <br> with user-friendly, drag and drop flowchart creations<br>& effortlessly design professional diagrams in minute</p>
+				<p>Let's start to make your own flowchart</p>
+				<button>Let's Make</button>
+			</div>
+	
+			<div class="right" id="flowchart"></div>
+			<div class="SocialMedia">
+				<i class="fa-brands fa-facebook-f"></i>
+				<i class="fa-brands fa-twitter"></i>
+				<i class="fa-brands fa-instagram"></i>
+				<i class="fa-brands fa-github"></i>
+			</div>
+		</div>
+	 </div>
 </div>
 
 <style>

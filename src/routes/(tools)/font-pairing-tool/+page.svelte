0bbe096c405364e7adc4089d78ff5407cf66fd76
aditya <72554602+adityapol41c@users.noreply.github.com>
2024-07-06 16:51:33 +0530

    new commit

--- src/routes/(tools)/font-pairing-tool/+page.svelte
+++ src/routes/(tools)/font-pairing-tool/+page.svelte
@@ -1,25 +1,13 @@
 <script>
-<<<<<<< Updated upstream
-	import PairingGenerator from "./PairingGenerator.svelte";
-=======
-	import PairingGenerator from './PairingGenerator.svelte';
->>>>>>> Stashed changes
+	import PairingGenerator from "./PairingGenerator.svelte";	
 </script>
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	
 </div>
-<<<<<<< Updated upstream
-
-<main>
-	<PairingGenerator />
-  </main>
-
-=======
 <main>
 	<PairingGenerator />
   </main>	
->>>>>>> Stashed changes
 <style>
 	main {
 	  padding: 20px;
--- src/routes/(tools)/font-pairing-tool/FontDisplay.svelte
+++ src/routes/(tools)/font-pairing-tool/FontDisplay.svelte
@@ -1,5 +1,4 @@
 <script>
-<<<<<<< Updated upstream
     export let font1;
     export let font2;
     export let text;
@@ -31,29 +30,3 @@
         
     }
   </style>
-=======
-  export let font1;
-  export let font2;
-  export let text;
-  export let size;
-  export let weight;
-</script>
-
-<div class="font-display">
-  <div style="font-family: {font1}; font-size: {size}px; font-weight: {weight};">{text}</div>
-  <div style="font-family: {font2}; font-size: {size}px; font-weight: {weight};">{text}</div>
-</div>
-
-<style>
-  .font-display {
-    margin-top: 20px;
-  }
-  .font-display > div {
-    margin-bottom: 10px;
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    background-color: #fff;
-  }
-</style>
->>>>>>> Stashed changes

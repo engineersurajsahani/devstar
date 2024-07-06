0bfa0660c21c087a5af10396bc5aee0b7b839dd4
aditya <72554602+adityapol41c@users.noreply.github.com>
2024-07-06 15:47:25 +0530

    comiited

--- src/routes/(tools)/font-pairing-tool/+page.svelte
+++ src/routes/(tools)/font-pairing-tool/+page.svelte
@@ -1,15 +1,25 @@
 <script>
+<<<<<<< Updated upstream
 	import PairingGenerator from "./PairingGenerator.svelte";
+=======
+	import PairingGenerator from './PairingGenerator.svelte';
+>>>>>>> Stashed changes
 </script>
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
+	
 </div>
+<<<<<<< Updated upstream
 
 <main>
 	<PairingGenerator />
   </main>
 
+=======
+<main>
+	<PairingGenerator />
+  </main>	
+>>>>>>> Stashed changes
 <style>
 	main {
 	  padding: 20px;

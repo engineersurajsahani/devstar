2650de4b7ac0a3ddc1d226d1e501dcb8618debb9
jasmitha <jashjk82@gmail.com>
2024-07-26 13:30:45 +0530

    change integer toolkit added

--- src/routes/(tools)/integer-toolkit/+page.svelte
+++ src/routes/(tools)/integer-toolkit/+page.svelte
@@ -286,6 +286,42 @@
       sortedIntegers = "";
     }
   }
+
+  // variable for integer sign changer
+  let input = '';
+  let mode = 'default';
+  let forceSign = false;
+  let output = '';
+  
+  function processInput() {
+    let numbers = input.split('\n').map(Number);
+    let processedNumbers ;
+
+    if(mode === 'default'){
+      processedNumbers = defaulte(numbers);
+    } else if (mode === 'minus'){
+      processedNumbers = minus(numbers);
+    }else if (mode === 'plus'){
+      processedNumbers = plus(numbers);
+    }
+
+    if (forceSign) {
+      processedNumbers = processedNumbers.map(n => n >= 0 ? `+${n}` : `${n}`);
+    }
+
+    output = processedNumbers.join('\n');
+  }
+  function defaulte(numbers){
+    return numbers.map(n => n === 0 ? 0 : -n);
+  }
+  function minus(numbers){
+    return numbers.map(n => -Math.abs(n));
+  }
+  function plus(numbers){
+    return numbers.map(n => Math.abs(n));
+  }
+  $: processInput();
+
 </script>
 
 <div class="tools-container space-y-8">
@@ -849,6 +885,128 @@
   </div>
 </div>
 
+<!-- Integer Sign Changer -->
+<div class="tool">
+  <h1 class="text-gray-900 text-2xl dark:text-white ml-12 font-medium">
+    5. Integer Sign Changer
+  </h1>
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-12">
+    <div
+      class="card p-8 relative items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg"
+    >
+      <div class="flex gap-8 mb-4">
+        <label class="flex flex-col items-center">
+          <input
+            type="radio"
+            name="mode"
+            value="default"
+            bind:group={mode} 
+          />
+          <span class="text-gray-900 text-base dark:text-white"
+            >Flip Sign</span
+          >
+          <span class="text-sm text-gray-500 dark:text-gray-400"
+            >Use opposite sign for all integers.</span
+          >
+        </label>
+        <label class="flex flex-col items-center">
+          <input
+            type="radio"
+            name="mode"
+            value="minus"
+            bind:group={mode} 
+          />
+          <span class="text-gray-900 text-base dark:text-white"
+            >Minus Sign</span
+          >
+          <span class="text-sm text-gray-500 dark:text-gray-400"
+            >Use minus sign for all integers.</span
+          >
+        </label>
+        <label class="flex flex-col items-center">
+          <input
+            type="radio"
+            name="mode"
+            value="plus"
+            bind:group={mode} 
+          />
+          <span class="text-gray-900 text-base dark:text-white"
+            >Plus Sign</span
+          >
+          <span class="text-sm text-gray-500 dark:text-gray-400"
+            >Use plus sign for all integers.</span
+          >
+        </label>
+        <label
+            class="text-base text-gray-500 dark:text-gray-100 flex items-center"
+          >
+            <input type="checkbox" bind:checked={forceSign} />
+            Force Sign
+          </label>
+      </div>
+
+      <div
+        class="mt-4 gap-4 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden"
+      >
+        <div
+          class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 p-4"
+        >
+          <h2 class="text-xl font-medium text-gray-700 dark:text-white mb-2">
+            Input Integers
+          </h2>
+          <textarea
+            placeholder="Enter Integers"
+            rows="8"
+            class="resize-none block p-2.5 w-full text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            bind:value={input}
+          ></textarea>
+        </div>
+
+        <div
+          class="rounded-lg overflow-hidden bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 p-4"
+        >
+          <h2 class="text-xl font-medium text-gray-700 dark:text-white mb-2">
+           Integers with Opposite Sign
+          </h2>
+          <textarea
+            placeholder="Result"
+            rows="8"
+            class="resize-none block p-2.5 w-full text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            bind:value={output}
+            readonly
+          ></textarea>
+        </div>
+      </div>
+
+      <!-- Buttons -->
+      <div
+        class="items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 overflow-hidden"
+      >
+        <div
+          class="mt-8 gap-4 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden"
+        >
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            on:click={processInput}>Process
+          
+          </button>
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            on:click={() =>
+              downloadText(output, "processednumber.txt")}
+          >
+            Download as txt
+          </button>
+          <!-- <button
+class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+>
+Download as pdf
+</button> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <style>
   .card h2 {
     padding: 0.5rem 0;

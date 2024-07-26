5821593b322b594976a76b6b2e9af19c150d680e
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-07-26 22:42:03 +0530

    collaborators name

--- src/routes/(tools)/youtube-thumbnail-grabber/+page.svelte
+++ src/routes/(tools)/youtube-thumbnail-grabber/+page.svelte
@@ -1,11 +1,56 @@
 <script>
+    let url = '';
+    let thumbnailUrl = '';
 
+    function getVideo(url) {
+        const urlObj = new URL(url);
+        const videoId = urlObj.searchParams.get('v');
+        if (videoId) {
+            return videoId;
+        }
+        // Handle case for shortened URLs or other formats
+        const pathSegments = urlObj.pathname.split('/');
+        return pathSegments[pathSegments.length - 1];
+    }
+
+    function getThumbnail() {
+        const videoId = getVideo(url);
+        if (videoId) {
+            thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+        } else {
+            thumbnailUrl = '';
+        }
+    }
 </script>
-
+<main>
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
-</div>
+
+<h1>YouTube Thumbnail Grabber</h1>
+    <input
+        type="text"
+        placeholder="Enter the URL of Youtube video"
+        bind:value={url}
+        on:input={getThumbnail}
+    />
+    {#if thumbnailUrl}
+        <img src={thumbnailUrl} alt="YouTube Thumbnail" />
+    {/if}
+</main>
+
 
 <style>
 
+    main {
+        text-align: center;
+        margin: 1em;
+        font-family: Arial, sans-serif;
+    }
+
+    input {
+        width: 80%;
+        padding: 0.5em;
+        margin: 0.5em;
+        font-size: 1em;
+    }
 </style>
